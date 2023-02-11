b177554b579bacef20589b9b24506470df65fd20
CALVINCE <calvince.ogwel@gmail.com>
2023-02-11 20:15:21 +0300

    completed mass assignmet lab

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,21 @@
 class Person
-  # your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+                :weight, :handed, :complexion, :t_shirt_size,
+                :wrist_size, :glove_size, :pant_length, :pant_width
+
+  def initialize(attributes)
+    @name = attributes[:name]
+    @birthday = attributes[:birthday]
+    @hair_color = attributes[:hair_color]
+    @eye_color = attributes[:eye_color]
+    @height = attributes[:height]
+    @weight = attributes[:weight]
+    @handed = attributes[:handed]
+    @complexion = attributes[:complexion]
+    @t_shirt_size = attributes[:t_shirt_size]
+    @wrist_size = attributes[:wrist_size]
+    @glove_size = attributes[:glove_size]
+    @pant_length = attributes[:pant_length]
+    @pant_width = attributes[:pant_width]
+  end
 end
